884fde10c8de859081e7dee0449d4f1a4b9e9411
nik0sc <nik0sc@users.noreply.github.com>
2022-08-14 17:45:53 +0800

    remove race in test

--- doneq/done_test.go
+++ doneq/done_test.go
@@ -1,6 +1,7 @@
 package doneq
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -12,29 +13,37 @@ import (
 func TestDone(t *testing.T) {
 	acks := make([]int, 0, 10)
 	goroutineExitOrder := make(chan int, 2)
+	var wg sync.WaitGroup
 
 	d := New(10, func(i int) {
 		acks = append(acks, i)
 	})
 
 	one := d.Start(1)
+	wg.Add(1)
 	go func() {
 		time.Sleep(time.Second)
 		one.Done()
 		goroutineExitOrder <- 1
+		wg.Done()
 	}()
 
 	two := d.Start(2)
+	wg.Add(1)
 	go func() {
 		time.Sleep(100 * time.Millisecond)
 		two.Done()
 		goroutineExitOrder <- 2
+		wg.Done()
 	}()
 
 	d.ShutdownWait()
 	assert.EqualValues(t, []int{1, 2}, acks)
+
+	wg.Wait()
 	close(goroutineExitOrder)
 	testutils.Drain(t, []int{2, 1}, goroutineExitOrder)
+
 	goleak.VerifyNone(t)
 }
 
