3d66964cd005a480c3d14a6c22dfe49d182879b2
nik0sc <nik0sc@users.noreply.github.com>
2023-01-13 11:14:35 +0800

    avoid race condition between use and eviction

--- dispatcher/lazy.go
+++ dispatcher/lazy.go
@@ -1,7 +1,9 @@
 package dispatcher
 
 import (
+	"fmt"
 	"sync"
+	"sync/atomic"
 
 	"go.lepak.sg/playground/slidingwindow"
 )
@@ -10,12 +12,14 @@ const (
 	defaultWindow = 100
 )
 
+// Keyer is the interface of items that Lazy accepts.
 type Keyer interface {
 	Key() string
 }
 
-type Acceptor[T Keyer] interface {
-	Accept(T)
+// Acceptor is the interface that Lazy will route Keyers to.
+type Acceptor interface {
+	Accept(Keyer) error
 	Close()
 }
 
@@ -26,18 +30,41 @@ type counter interface {
 	Observe(string)
 }
 
-type Lazy[T Keyer] struct {
-	lock    sync.RWMutex
-	active  map[string]Acceptor[T]
-	window  counter
-	factory func(string) Acceptor[T]
+type acceptorEntry struct {
+	acceptor Acceptor
+	// refcount is atomically incremented when retrieving
+	// this acceptorEntry from Lazy.active
+	// then decremented after acceptor is used
+	refCount int64
 }
 
-func NewLazy[T Keyer](
-	factory func(string) Acceptor[T], windowSize, keyCardinality int,
-) *Lazy[T] {
-	ld := &Lazy[T]{
-		active:  make(map[string]Acceptor[T]),
+type Lazy struct {
+	active  map[string]*acceptorEntry
+	window  counter
+	factory func(string) Acceptor
+	// protects active and window (the reference to LockedCounter)
+	// not allowed to hold this and then call window methods
+	lock sync.RWMutex
+}
+
+// It should be possible to chain Lazys together
+var _ Acceptor = (*Lazy)(nil)
+
+// NewLazy creates a lazy dispatcher.
+// factory is the function called to create a new Acceptor for
+// a given key. windowSize is the number of items accepted without using
+// an Acceptor before it is considered idle and closed. keyCardinality
+// is a guess for how many unique keys there are (it may be 0).
+//
+// Note that factory may be called from multiple goroutines at once.
+// Also, an Acceptor may be created and then closed without ever
+// being used.
+// Once an Acceptor is no longer needed, its Close method is called.
+func NewLazy(
+	factory func(string) Acceptor, windowSize, keyCardinality int,
+) *Lazy {
+	ld := &Lazy{
+		active:  make(map[string]*acceptorEntry),
 		factory: factory,
 	}
 
@@ -51,7 +78,24 @@ func NewLazy[T Keyer](
 	return ld
 }
 
-func (ld *Lazy[T]) Accept(item T) {
+func (ld *Lazy) newAcceptor(key string) (ac Acceptor, err error) {
+	defer func() {
+		switch r := recover().(type) {
+		case error:
+			err = fmt.Errorf("factory paniced: %w", r)
+		case nil:
+			return
+		default:
+			err = fmt.Errorf("factory paniced: %v", r)
+		}
+	}()
+	ac = ld.factory(key)
+	return
+}
+
+// Accept accepts a keyable item for dispatching.
+// Any error from the acceptor is returned.
+func (ld *Lazy) Accept(item Keyer) error {
 	key := item.Key()
 
 	ld.lock.RLock()
@@ -61,9 +105,24 @@ func (ld *Lazy[T]) Accept(item T) {
 	}
 
 	dest, ok := ld.active[key]
+	if ok {
+		// prevent ld.window.Observe -> (evict) -> ld.cleanup
+		// from closing this acceptor in another goroutine.
+		// ld.cleanup acquires the full r/w lock before checking refcount,
+		// but this increment happens while ld.Accept holds the rlock,
+		// so it's not possible for ld.cleanup to see refcount = 0
+		// while ld.Accept is using the acceptor
+		atomic.AddInt64(&dest.refCount, 1)
+	}
 	ld.lock.RUnlock()
 
 	if !ok {
+		// avoid calling the factory while holding lock
+		acceptor, err := ld.newAcceptor(key)
+		if err != nil {
+			return err
+		}
+
 		ld.lock.Lock()
 		if ld.window == nil {
 			ld.lock.Unlock()
@@ -72,37 +131,59 @@ func (ld *Lazy[T]) Accept(item T) {
 
 		dest, ok = ld.active[key]
 		if !ok {
-			dest = ld.factory(key)
+			dest = &acceptorEntry{
+				acceptor: acceptor,
+				refCount: 1, // using it now
+			}
 			ld.active[key] = dest
+		} else {
+			atomic.AddInt64(&dest.refCount, 1)
 		}
 		ld.lock.Unlock()
+
+		if ok {
+			// close the unnecessary acceptor that was
+			// just created
+			acceptor.Close()
+		}
 	}
 
 	ld.window.Observe(key)
-	dest.Accept(item)
+	err := dest.acceptor.Accept(item)
+	atomic.AddInt64(&dest.refCount, -1)
+	return err
 }
 
-func (ld *Lazy[T]) Close() {
+func (ld *Lazy) Close() {
 	ld.lock.Lock()
 	defer ld.lock.Unlock()
 
 	ld.window = nil
 	for _, dest := range ld.active {
-		dest.Close()
+		dest.acceptor.Close()
 	}
 }
 
-func (ld *Lazy[T]) cleanup(key string) {
-	// full write lock must be held! do this in the background
-	// otherwise Lazy.Accept will deadlock with its own cleanup
-	go func() {
-		ld.lock.Lock()
-		dest, ok := ld.active[key]
-		delete(ld.active, key)
+func (ld *Lazy) cleanup(key string) {
+	ld.lock.Lock()
+	dest, ok := ld.active[key]
+	if !ok {
 		ld.lock.Unlock()
+		panic("key already removed")
+	}
+	if atomic.LoadInt64(&dest.refCount) != 0 {
+		ld.lock.Unlock()
+		// This is tricky: the key has already left the window
+		// but the acceptor should not be removed from ld.active.
+		// Trust that the other goroutine will Observe this key also,
+		// therefore putting the key back in the window.
+		// print("warning: refcount was not zero", dest)
+		return
+	}
+	delete(ld.active, key)
+	ld.lock.Unlock()
 
-		if ok && dest != nil {
-			dest.Close()
-		}
-	}()
+	if dest != nil {
+		dest.acceptor.Close()
+	}
 }
---
+++ dispatcher/lazy_test.go
@@ -0,0 +1,229 @@
+package dispatcher
+
+import (
+	"errors"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	ctr "go.lepak.sg/playground/counter"
+	"go.lepak.sg/playground/testutils"
+	"golang.org/x/exp/maps"
+)
+
+type stringKeyer struct {
+	value string
+}
+
+func (s *stringKeyer) Key() string {
+	return s.value[:1]
+}
+
+type stringEvent struct {
+	id    uint64
+	value string
+}
+
+type stringAcceptor struct {
+	t      *testing.T
+	id     uint64
+	key    string
+	ev     chan stringEvent
+	closed uint64
+}
+
+func (a *stringAcceptor) Accept(item Keyer) error {
+	if atomic.LoadUint64(&a.closed) == 1 {
+		return errors.New("closed")
+	}
+
+	k, ok := item.(*stringKeyer)
+	if !ok {
+		return errors.New("wrong type")
+	}
+
+	if k.Key() != a.key {
+		return errors.New("wrong key")
+	}
+
+	a.ev <- stringEvent{a.id, k.value}
+
+	return nil
+}
+
+func (a *stringAcceptor) Close() {
+	a.t.Logf("closing: key=%q id=%d", a.key, a.id)
+	if !atomic.CompareAndSwapUint64(&a.closed, 0, 1) {
+		a.t.Logf("already closed! key=%q id=%d", a.key, a.id)
+	}
+}
+
+const (
+	seed = 58913
+)
+
+func TestLazy(t *testing.T) {
+	tests := []struct {
+		name           string
+		windowSize     int
+		keyCardinality int
+		chanSize       int
+		do             func(t *testing.T, l *Lazy)
+		after          func(t *testing.T, ch chan stringEvent)
+	}{
+		{
+			name:           "empty",
+			windowSize:     1,
+			keyCardinality: 1,
+			chanSize:       1,
+			do: func(t *testing.T, l *Lazy) {
+			},
+			after: func(t *testing.T, ch chan stringEvent) {
+				assert.Len(t, ch, 0)
+			},
+		},
+		{
+			name:           "single",
+			windowSize:     1,
+			keyCardinality: 1,
+			chanSize:       3,
+			do: func(t *testing.T, l *Lazy) {
+				acceptMany(t, l, "apple", "banana", "apricot")
+			},
+			after: func(t *testing.T, ch chan stringEvent) {
+				testutils.Drain(t, []stringEvent{
+					{1, "apple"},
+					{2, "banana"},
+					{3, "apricot"},
+				}, ch)
+			},
+		},
+		{
+			name:           "more lazy",
+			windowSize:     2,
+			keyCardinality: 3,
+			chanSize:       10,
+			do: func(t *testing.T, l *Lazy) {
+				acceptMany(t, l,
+					"apple",
+					"banana",
+					"apricot",
+					"blueberry",
+					"cherry",
+					"blackcurrant",
+					"avocado",
+					"breadfruit",
+				)
+			},
+			after: func(t *testing.T, ch chan stringEvent) {
+				testutils.Drain(t, []stringEvent{
+					{1, "apple"},
+					{2, "banana"},
+					{1, "apricot"},
+					{2, "blueberry"},
+					{3, "cherry"},
+					{2, "blackcurrant"},
+					{4, "avocado"},
+					{2, "breadfruit"},
+				}, ch)
+			},
+		},
+		{
+			name:           "concurrent",
+			windowSize:     10,
+			keyCardinality: 4,
+			chanSize:       10200,
+			do: func(t *testing.T, l *Lazy) {
+				fruits := []string{
+					"apple", "apricot", "avocado",
+					"banana", "blueberry", "blackcurrant",
+					"cherry", "coconut", "cantaloupe",
+					"durian", "date", "dragonfruit",
+				}
+
+				const (
+					goroutines = 3
+					iterations = 10000
+				)
+
+				rand.Seed(seed)
+
+				count := new(uint64)
+				barrier := make(chan struct{})
+				var wg sync.WaitGroup
+
+				wg.Add(goroutines)
+				for i := 0; i < goroutines; i++ {
+					go func() {
+						<-barrier
+						for {
+							fruit := fruits[rand.Intn(len(fruits))]
+							err := l.Accept(&stringKeyer{fruit})
+							assert.NoError(t, err)
+							if atomic.AddUint64(count, 1) >= iterations {
+								wg.Done()
+								return
+							}
+						}
+					}()
+				}
+
+				close(barrier)
+				wg.Wait()
+			},
+			after: func(t *testing.T, ch chan stringEvent) {
+				events := make([]stringEvent, 0, len(ch))
+				for e := range ch {
+					events = append(events, e)
+				}
+
+				c := ctr.Counter(events)
+				k := 100
+				if len(c) < k {
+					k = len(c)
+				}
+				top := ctr.TopK(c, k)
+				for _, e := range top {
+					t.Log(e)
+				}
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			outchan := make(chan stringEvent, tt.chanSize)
+
+			id := new(uint64)
+
+			l := NewLazy(func(s string) Acceptor {
+				return &stringAcceptor{
+					t:   t,
+					id:  atomic.AddUint64(id, 1),
+					key: s,
+					ev:  outchan}
+			}, tt.windowSize, tt.keyCardinality)
+
+			tt.do(t, l)
+			l.Close()
+			close(outchan)
+			tt.after(t, outchan)
+		})
+	}
+}
+
+func checkLazy(t *testing.T, l *Lazy, ss string) {
+	all := l.window.(interface{ GetAll() map[string]int }).GetAll()
+	w := maps.Clone(l.active)
+
+	assert.ElementsMatchf(t, maps.Keys(all), maps.Keys(w),
+		"map broken after accepting %q", ss)
+}
+
+func acceptMany(t *testing.T, l *Lazy, s ...string) {
+	for _, ss := range s {
+		l.Accept(&stringKeyer{ss})
+		checkLazy(t, l, ss)
+	}
+}
